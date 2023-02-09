88ee9a354c2fb09c94cf7ce5b5ce76866c40d6fe
sibela46 <sibelachinareva@gmail.com>
2023-02-09 13:11:50 +0200

    Ray tracing a sphere

--- CashewApp/src/WalnutApp.cpp
+++ CashewApp/src/WalnutApp.cpp
@@ -4,6 +4,8 @@
 #include "Walnut/Image.h"
 #include "Walnut/Timer.h"
 
+#include "Renderer.h"
+
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
@@ -27,37 +29,29 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		if (m_Image)
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
 		{
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() }, ImVec2(0, 1), ImVec2(1, 0));
 		}
 
 		ImGui::End();
 		ImGui::PopStyleVar();
+
+		Render();
 	}
 	void Render()
 	{
 		Timer timer;
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-
-		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-		{
-			m_ImageData[i] = 0xffff00ff;
-		}
-		
-		m_Image->SetData(m_ImageData);
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
-	uint32_t* m_ImageData = nullptr;
+	Renderer m_Renderer;
+	uint32_t* m_FinalImageData = nullptr;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 	float m_LastRenderTime = 0;
 };
